941a35ca8610ec2fe4115544c3335123006f7dde
harshvir098 <harshvirsingh505@gmail.com>
2024-11-27 19:14:35 +0100

    Endpoint de autonomias

--- src/main/java/com/proyectofinal/config/SecurityConfig.java
+++ src/main/java/com/proyectofinal/config/SecurityConfig.java
@@ -54,7 +54,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests((requests) -> {
                     try {
                         // Definimos que urls estarán desprotegidas y no necesitarán recibir las credenciales para poder ser accedidas
-                        requests.requestMatchers("/auth/register").permitAll().anyRequest().authenticated();
+                        requests .requestMatchers("/api/autonomies").permitAll()  // Allow access to the autonomies endpoint
+                        .requestMatchers("/auth/register").permitAll() 
+                        .anyRequest().authenticated();
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
--- src/main/java/com/proyectofinal/controllers/AutonomyController.java
+++ src/main/java/com/proyectofinal/controllers/AutonomyController.java
@@ -1,27 +1,33 @@
 package com.proyectofinal.controllers;
 
-
-import com.proyectofinal.persistence.entities.Autonomy;
-import com.proyectofinal.service.AutonomyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyectofinal.persistence.entities.Autonomy;
+import com.proyectofinal.persistence.repositories.AutonomyRepository;
+
 @RestController
+@RequestMapping("/api/autonomies")
+@CrossOrigin
 public class AutonomyController {
 
-    @Autowired
-    private AutonomyService autonomyService;
-    @Autowired
-   
+    private final AutonomyRepository autonomyRepository;
 
-    // Endpoint to get all autonomies
-    @GetMapping("/autonomies")
+    // Constructor injection to inject AutonomyRepository
+    @Autowired
+    public AutonomyController(AutonomyRepository autonomyRepository) {
+        this.autonomyRepository = autonomyRepository;
+    }
+
+    // Get all autonomies with location, latitude, and longitude
+    @GetMapping
     public List<Autonomy> getAllAutonomies() {
-        return autonomyService.getAllAutonomies();
+        // Fetching all autonomies from the repository
+        return autonomyRepository.findAll();
     }
 }
-
